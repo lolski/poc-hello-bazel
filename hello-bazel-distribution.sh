38dbca2e42d31fddd5a8e339adcbd38df4a2b839
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-10-07 20:04:01 +0100

    wip - genrule

---
+++ hello-bazel-distribution.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+script="$1"
+jar="$2"
+output="$3"
+
+base_dir=hello-bazel-distribution
+jar_dir=jar
+
+mkdir "$base_dir"
+mkdir "$base_dir/$jar_dir"
+cp "$script" "$base_dir"
+cp "$jar" "$base_dir/$jar_dir"
+
+zip -r "$output" "$base_dir"
