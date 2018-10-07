e48420ae0614d30537c217a9ff66085bb06e5eec
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-10-07 14:49:37 +0100

    added guava jar dependency

---
+++ WORKSPACE
@@ -0,0 +1,8 @@
+workspace(
+    name = "hello_bazel"
+)
+
+maven_jar(
+    name = "com_google_guava_guava",
+    artifact = "com.google.guava:guava:23.0"
+)
--- package-a/src/main/java/com/lolski/packagea/Main.java
+++ package-a/src/main/java/com/lolski/packagea/Main.java
@@ -1,9 +1,18 @@
 package com.lolski.packagea;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import com.lolski.packageb.logger.Logger;
 
 public class Main {
     public static void main(String args[]) {
+        Multimap<String, String> mmap = ArrayListMultimap.create();
+        mmap.put("k1", "v1");
+        mmap.put("k1", "v2");
+        mmap.put("k1", "v3");
+        for (String key: mmap.keySet()) {
+            Logger.log("values = " + mmap.get(key));
+        }
         Logger.log("Hi!");
     }
 }
